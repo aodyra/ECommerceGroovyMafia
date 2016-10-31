57a4dbcc476470952251e879003dc5e7f5dffce3
squilliams <bimoaryo@rocketmail.com>
2016-10-31 15:52:59 +0700

    Menambahkan kelas-kelas yang diperlukan

---
+++ src/ecommercegroovy/jenisbisnis.groovy
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ecommercegroovy
+
+/**
+ *
+ * @author Bimo
+ */
+enum jenisbisnis {
+	b2b,
+        b2c
+}
+
