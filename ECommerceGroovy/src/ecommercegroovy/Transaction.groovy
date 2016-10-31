77636c8cc1576347db92a6e6fcf7e16d67928df1
miftahulmahfuzh <mahfuzh74@gmail.com>
2016-10-31 16:09:05 +0700

    Transaction class added

---
+++ ECommerceGroovy/src/ecommercegroovy/Transaction.groovy
@@ -0,0 +1,24 @@
+class Transaction
+{
+  def wholesale
+
+  public Transaction()
+  {
+    this.wholesale = 0;
+  }
+
+  public Transaction(int wholesale)
+  {
+    this.wholesale = wholesale;
+  }
+
+  public void addWholesale(int wholesale)
+  {
+    this.wholesale = wholesale;
+  }
+
+  public int getWholesale()
+  {
+    return this.wholesale;
+  }
+}
